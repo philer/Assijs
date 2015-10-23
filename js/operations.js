94e4305bc1f60d4d736a32c60ec66cbed5ef9bb1
Philipp Miller <philipp.miller@t-online.de>
2015-10-23 16:20:46 +0200

    added more JMP* operations

--- js/operations.js
+++ js/operations.js
@@ -3,23 +3,77 @@ var OPERATIONS = {
   // Controll
   
   HOLD: {
-    code: 00,
+    code: 0,
     callback: function() {
       return true;
     },
   },
   
   JMP: {
-    code: 01,
+    code: 1,
     callback: function(target) {
       this.counter.set(target - 1);
     },
   },
   
   JMPZ: {
-    code: 02,
+    code: 2,
     callback: function(target) {
       if (this.zFlag.get()) {
+        this.counter.set(target - 1);
+      }
+    },
+  },
+  
+  JMPNZ: {
+    code: 3,
+    callback: function(target) {
+      if (!this.zFlag.get()) {
+        this.counter.set(target - 1);
+      }
+    },
+  },
+  
+  JMPN: {
+    code: 4,
+    callback: function(target) {
+      if (this.nFlag.get()) {
+        this.counter.set(target - 1);
+      }
+    },
+  },
+  
+  JMPNN: {
+    code: 5,
+    callback: function(target) {
+      if (!this.nFlag.get()) {
+        this.counter.set(target - 1);
+      }
+    },
+  },
+  
+  JMPP: {
+    code: 6,
+    callback: function(target) {
+      if (!(this.zFlag.get() || this.nFlag.get())) {
+        this.counter.set(target - 1);
+      }
+    },
+  },
+  
+  JMPNP: {
+    code: 7,
+    callback: function(target) {
+      if (this.zFlag.get() || this.nFlag.get()) {
+        this.counter.set(target - 1);
+      }
+    },
+  },
+  
+  JMPV: {
+    code: 8,
+    callback: function(target) {
+      if (this.vFlag.get()) {
         this.counter.set(target - 1);
       }
     },
